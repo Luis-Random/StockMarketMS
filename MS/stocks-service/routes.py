17391da159928667204a12a64e93b370f67b3508
Luis-Random <62096892+Luis-Random@users.noreply.github.com>
2025-06-13 00:11:04 +0100

    Update routes.py
    Integração de Factories e Facade

--- MS/stocks-service/routes.py
+++ MS/stocks-service/routes.py
@@ -4,13 +4,15 @@ from fastapi.responses import HTMLResponse  # type: ignore
 from fastapi.templating import Jinja2Templates  # type: ignore
 from Controllers.StockController import *
 from Models.Stock import Stock # type: ignore
+from factories.stock_factories import StockFactories
 
 router = APIRouter()
 templates = Jinja2Templates(directory="views")
+stock = StockFactories.create_stock_service()
 
 @router.get("/stocks-page", response_class=HTMLResponse)
 async def stocks_page(request: Request):
-    stocks = await list_stocks()
+    stocks = await stock.list_stocks()
     return templates.TemplateResponse("stock_index.html", {"request": request, "stocks": stocks})
 
 
@@ -20,24 +22,24 @@ async def stocks_create_page(request: Request):
 
 @router.post("/stocks")
 async def stocks_create(request: Request):
-    return await create_stock(request)
+    return await stock.create_stock(request)
 
 @router.get("/edit/{stock_id}", response_class=HTMLResponse)
 async def stocks_edit_page(request: Request, stock_id: str):
-    return await stock_edit(request, stock_id)
+    return await stock.stock_edit(request, stock_id)
 
 @router.post("/update/{stock_id}")
 async def stocks_update(request: Request, stock_id: str):
-    return await update_stock(request, stock_id)
+    return await stock.update_stock(request, stock_id)
 
 @router.post("/delete/{stock_id}")
 async def stocks_delete(stock_id: str):
-    return await delete_stock(stock_id)
+    return await stock.delete_stock(stock_id)
 
 
 @router.get("/api/stocks")
 async def api_stocks():
-    stocks = await list_stocks()
+    stocks = await stock.list_stocks()
     return [stock.__dict__ for stock in stocks]
 
 @router.post("/api/update-price")

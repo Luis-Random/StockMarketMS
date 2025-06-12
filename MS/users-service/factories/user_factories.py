7041d00d0e85d1b43272c952cc1f2b4e2f8d3257
Luis-Random <62096892+Luis-Random@users.noreply.github.com>
2025-06-13 00:09:13 +0100

    Create user_factories.py
    Factory e Facade para o user-service

---
+++ MS/users-service/factories/user_factories.py
@@ -0,0 +1,30 @@
+from urllib.request import Request
+
+from Controllers.UserController import *
+from Models.User import User
+class UserFactories:
+    @staticmethod
+    def create_user_service():
+        return UserFacade()
+class UserFacade:
+    @staticmethod
+    async def create_user():
+        return list_users()
+    @staticmethod
+    async def list_users():
+        return list_users()
+    @staticmethod
+    async def get_user(user_id:str):
+        return get_user(user_id)
+    @staticmethod
+    async def user_edit(user_id:str):
+        return user_edit(user_id)
+    @staticmethod
+    async def update_user(request:Request,user_id:str):
+        return update_user(request,user_id)
+    @staticmethod
+    async def delete_user(user_id:str):
+        return delete_user(user_id)
+    @staticmethod
+    async def transfer_balance(request:Request):
+        return transfer_balance(request)

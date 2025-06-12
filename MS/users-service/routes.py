23eb026f06f1f4d12079c0bd1091b6dd62b1801a
Luis-Random <62096892+Luis-Random@users.noreply.github.com>
2025-06-13 00:10:17 +0100

    Update routes.py
    Integração de Factory e Facade

--- MS/users-service/routes.py
+++ MS/users-service/routes.py
@@ -1,20 +1,22 @@
 from fastapi import APIRouter, Request  # type: ignore
 from fastapi.responses import HTMLResponse  # type: ignore
 from fastapi.templating import Jinja2Templates  # type: ignore
-from Controllers.UserController import *
+#from Controllers.UserController import *
+from factories.user_factories import UserFactories
 
 router = APIRouter()
 templates = Jinja2Templates(directory="views")
+userFactory = UserFactories.create_user_service()
 
 router.add_api_route("/create-user", create_user, methods=["POST"])
 router.add_api_route("/get-user/{user_id}", get_user, methods=["GET"])
 @router.post("/api/transfer")
 async def api_transfer_balance(request: Request):
-    return await transfer_balance(request)
+    return await userFactory.transfer_balance(request)
 
 @router.get("/users-page", response_class=HTMLResponse)
 async def users_page(request: Request):
-    users = await list_users()
+    users = await userFactory.list_users()
     return templates.TemplateResponse("user_index.html", {"request": request, "users": users})
 
 @router.get("/create", response_class=HTMLResponse)
@@ -25,18 +27,18 @@ async def users_create_page(request: Request):
 
 @router.get("/edit/{user_id}", response_class=HTMLResponse)
 async def edit_user_view(request: Request, user_id: str):
-    return await user_edit(request, user_id)
+    return await userFactory.user_edit(request, user_id)
 
 @router.post("/update-user/{user_id}")
 async def update_user_route(request: Request, user_id: str):
-    return await update_user(request, user_id)
+    return await userFactory.update_user(request, user_id)
 
 @router.post("/delete-user/{user_id}")
 async def delete_user_route(user_id: str):
-    return await delete_user(user_id)
+    return await userFactory.delete_user(user_id)
 
 
 @router.get("/api/users")
 async def api_users():
-    users = await list_users()
+    users = await userFactory.list_users()
     return [user.__dict__ for user in users]

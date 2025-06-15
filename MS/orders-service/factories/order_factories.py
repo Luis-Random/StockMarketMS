130e6074c4c112f587ce265077c42b635cff41a0
Luis-Random <62096892+Luis-Random@users.noreply.github.com>
2025-06-15 12:19:12 +0100

    Update order_factories.py

--- MS/orders-service/factories/order_factories.py
+++ MS/orders-service/factories/order_factories.py
@@ -7,29 +7,29 @@ class OrdersFactories:
 class OrderFacade:
      @staticmethod
      async def fetch_users():
-          return await orderInstance.fetch_users()
+          return await fetch_users()
      @staticmethod
      async def fetch_stocks():
-          return await orderInstance.fetch_stocks()
+          return await fetch_stocks()
      @staticmethod
      async def transfer_balance(buyer_id:str, seller_id:str, amount:float):
-          return await orderInstance.transfer_balance(buyer_id, seller_id, amount)
+          return await transfer_balance(buyer_id, seller_id, amount)
      @staticmethod
      async def list_orders():
-          return await orderInstance.list_orders()
+          return await list_orders()
      @staticmethod
      async def process_order_matching(order: Order):
-          return await orderInstance.process_order_matching(order)
+          return await process_order_matching(order)
      @staticmethod
      async def create_order(request:Request):
-          return await orderInstance.create_order(request)
+          return await create_order(request)
      @staticmethod
      async def order_edit(request:Request, order_id: str):
-          return await orderInstance.order_edit(request, order_id)
+          return await order_edit(request, order_id)
      @staticmethod
      async def update_order(request:Request, order_id: str):
-          return await orderInstance.update_order(request, order_id)
+          return await update_order(request, order_id)
      @staticmethod
      async def delete_order(order_id: str):
-          return await orderInstance.delete_order(order_id)
+          return await delete_order(order_id)
 

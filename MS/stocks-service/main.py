84329e7b12830ce2fb8bd4d531fd3a46d3fab4b7
Luis-Random <62096892+Luis-Random@users.noreply.github.com>
2025-06-16 21:13:06 +0100

    Update main.py

--- MS/stocks-service/main.py
+++ MS/stocks-service/main.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI  # type: ignore
-from fastapi.staticfiles import StaticFiles  # type: ignore
-from fastapi.templating import Jinja2Templates  # type: ignore
-from routes import router as user_router
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from routes import router as stocks_router
 
 app = FastAPI()
-app.include_router(user_router)
+app.include_router(stocks_router)
 
-# app.mount("/views", StaticFiles(directory="views"), name="views")  # REMOVIDO: views são templates, não ficheiros estáticos
 templates = Jinja2Templates(directory="views")

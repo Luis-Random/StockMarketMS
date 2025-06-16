9dde53a29730cc9188ac19ebc72c802edae17494
Luis-Random <62096892+Luis-Random@users.noreply.github.com>
2025-06-16 21:14:03 +0100

    Update stock_factories.py

--- MS/stocks-service/factories/stock_factories.py
+++ MS/stocks-service/factories/stock_factories.py
@@ -7,16 +7,16 @@ class StockFactories:
 class StockFacade:
     @staticmethod
     async def list_stocks():
-        return stock.all()
+        return await list_stocks()
     @staticmethod
     async def create_stock(request:Request):
-        return create_stock(request)
+        return await create_stock(request)
     @staticmethod
     async def stock_edit(request:Request, stock_id:str):
-        return stock_edit(request,stock_id)
+        return await stock_edit(request,stock_id)
     @staticmethod
     async def update_stock(request:Request, stock_id:str):
-        return update_stock(request,stock_id)
+        return await update_stock(request,stock_id)
     @staticmethod
     async def delete_stock(stock_id:str):
-        return delete_stock(stock_id)
+        return await delete_stock(stock_id)

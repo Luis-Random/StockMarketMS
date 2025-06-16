9b1bc06100959698ba78c17744d5fd72cd6d422f
Luis-Random <62096892+Luis-Random@users.noreply.github.com>
2025-06-16 20:56:19 +0100

    Update main.py

--- MS/users-service/main.py
+++ MS/users-service/main.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI  # type: ignore
-from fastapi.staticfiles import StaticFiles  # type: ignore
-from fastapi.templating import Jinja2Templates  # type: ignore
+from fastapi import FastAPI  
+from fastapi.staticfiles import StaticFiles 
+from fastapi.templating import Jinja2Templates  
 from routes import router as user_router
 
 app = FastAPI()
 app.include_router(user_router)
 
-# app.mount("/views", StaticFiles(directory="views"), name="views")  # REMOVIDO: views são templates, não ficheiros estáticos
 templates = Jinja2Templates(directory="views")
